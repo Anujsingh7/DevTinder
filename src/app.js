d5c6deae952784e40dc620f7770b6e79343d28fb
Anuj Kumar Singh <anujsinghpp123@gmail.com>
2024-12-27 17:40:58 +0530

    learning routing request

--- src/app.js
+++ src/app.js
@@ -2,6 +2,33 @@ const express = require("express");
 
 const app = express();
 
+/// queries param
+app.get("/queries",(req,res)=>{
+  console.log(req.query);
+  res.send({firstName: "John", lastName: "Sinha"});
+})
+
+
+// Dynamics routes 
+app.get("/dynamics/:first/:second/:third",(req,res)=>{
+  console.log(req.params);
+  res.send({firstName: "John", lastName: "Sinha"});
+})
+
+
+
+app.get("/user",(req,res)=>{
+  res.send({firstName: "John", lastName: "Sinha"});
+})
+app.post("/user",(req,res)=>{
+    res.send("Data stored in database successfully");
+})
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully");
+})
+
+// "use" this will match all HTTP requests methods calls
+
 app.use("/test", (req, res) => {
   res.send("aa re pritam pyare");
 });
@@ -12,4 +39,4 @@ app.use("/", (req, res) => {
   res.send("chal beta selfi lele re");
 });
 
-app.listen(8888, console.log("server is successfully listening"));
+app.listen(7777, console.log("server is successfully listening"));
